6d8b16a623ebe871eb4e7e4eea0672cd279ed0fb
lillie <lillie@Lillies-MacBook-Pro.local>
2021-07-06 16:22:03 -0700

    set up route on homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,37 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection.js');
+const { Article, User, Comment } = require('../models');
+
+
+router.get('/', (req,res) => {
+    Article.findAll({ 
+        attributes: ["id", "title", "content", 'created_at'], 
+    include: [
+        {model: Comment, 
+            attributes: ['id','article_id', 'user_id', 'content', 'created_at'], 
+        include: {model: User,
+        attributes: ['username']}
+    },
+    {model: User, attributes: ['username']}
+]})
+.then(articleData => {
+    const articles = articleData.map(article => article.get({plain: true}));
+    res.render('homepage', {
+        articles,
+        logged_in: req.session.logged_in
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+});
+
+
+
+
+
+
 
 
 
