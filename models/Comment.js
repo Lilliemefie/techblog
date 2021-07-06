17dca97cb67369609163b9d0816199ebe244941c
lillie <lillie@Lillies-MacBook-Pro.local>
2021-07-06 15:43:05 -0700

    updated models

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,46 @@
 //id, date create + use reference from User (name)
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1],
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+        article_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'article',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    });
+
+    module.exports = Comment; 
